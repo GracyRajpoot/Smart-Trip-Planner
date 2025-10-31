dee5b88d2ae80043dd0ecaad9b0c199489a2f20b
GracyRajpoot <gracyrajpoot90@gmail.com>
2025-10-31 10:27:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
+
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home'
 import Tours from './Pages/Tours'
 import Gallery from './Pages/Gallery'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Footer from './Components/Footer'
-import ScrollToTop from "react-scroll-to-top";
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element: <><Navbar/><Home/><Footer/></>
-  },
-  {
-    path:'/tours',
-    element: <><Navbar/><Tours/><Footer/></>
-  },
-  {
-    path:'/gallery',
-    element: <><Navbar/><Gallery/><Footer/></>
-  },
-  {
-    path:'/about',
-    element: <><Navbar/><About/><Footer/></>
-  },
-  {
-    path:'/contact',
-    element: <><Navbar/><Contact/><Footer/></>
-  },
-])
+import ScrollToTop from 'react-scroll-to-top'
 
 const App = () => {
   return (
     <>
-      <RouterProvider router={router}/>
-      <ScrollToTop color='white' smooth style={{backgroundColor:'#EF4444', display:'flex', alignItems:'center', justifyContent:'center'}}/>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tours" element={<Tours />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+
+      <ScrollToTop
+        color="white"
+        smooth
+        style={{
+          backgroundColor: '#EF4444',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      />
     </>
   )
 }
